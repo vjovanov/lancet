41ca9a67f5e9ca99e19ba2f9848607f32d640352
Vojin Jovanovic <vojin.jovanovic@epfl.ch>
2013-08-07 18:28:13 +0200

    StringOps completed.

--- src/main/scala/lancet/codegen/GraalGenStringOps.scala
+++ src/main/scala/lancet/codegen/GraalGenStringOps.scala
@@ -16,29 +16,52 @@ trait GraalGenStringOps extends GraalNestedCodegen with GraalBuilder {
   override def emitNode(sym: Sym[Any], rhs: Def[Any]) =  {
       rhs match {
         case StringPlus(s1,s2)       => ssa(sym) {
-          tpString(s2) match {
-            case "java.lang.String" =>
-              push(s1, s2)
-              invoke(classOf[String], "concat", classOf[String])
-            case _ =>
-              push(s1)
-              pushToString(s2)
-              invoke(classOf[String], "concat", classOf[String])
-          }
+          pushToString(s1, s2)
+          invoke(classOf[String], "concat", classOf[String])
         }
         case StringStartsWith(s1,s2) => ssa(sym) {
           push(s1, s2)
           invoke(classOf[String], "startsWith", classOf[String])
         }
-        case StringTrim(s)           => ???
-        case StringSplit(s, sep)     => ???
-        case StringEndsWith(s, e)    => ???
-        case StringCharAt(s,i)       => ???
-        case StringValueOf(a)        => ???
-        case StringToDouble(s)       => ???
-        case StringToFloat(s)        => ???
-        case StringToInt(s)          => ???
-        case StringContains(s1,s2)   => ???
+        case StringContains(s1,s2)   => ssa(sym) {
+          push(s1, s2)
+          invoke(classOf[String], "endsWith", classOf[String])
+        }
+        case StringSplit(s, sep)     => ssa(sym) {
+          push(s, sep)
+          invoke(classOf[String], "split", classOf[String])
+        }
+        case StringEndsWith(s, e)    => ssa(sym) {
+          push(s, e)
+          invoke(classOf[String], "endsWith", classOf[String])
+        }
+        case StringCharAt(s,i)       => ssa(sym) {
+          push(s, i)
+          invoke(classOf[String], "charAt", classOf[Int])
+        }
+        case StringTrim(s)           => ssa(sym) {
+          push(s)
+          invoke(classOf[String], "trim")
+        }
+        case StringValueOf(a)        => ssa(sym) {
+          push(Const(LancetString), a)
+          invoke(LancetString.getClass, "valueOf", classOf[String])
+        }
+        case StringToDouble(s)       => ssa(sym) {
+          push(Const(Predef), s)
+          invoke(Predef.getClass, "augmentString", classOf[String])
+          invoke(classOf[scala.collection.immutable.StringOps], "toDouble")
+        }
+        case StringToFloat(s)        => ssa(sym) {
+          push(Const(Predef), s)
+          invoke(Predef.getClass, "augmentString", classOf[String])
+          invoke(classOf[scala.collection.immutable.StringOps], "toFloat")
+        }
+        case StringToInt(s)          => ssa(sym) {
+          push(Const(Predef), s)
+          invoke(Predef.getClass, "augmentString", classOf[String])
+          invoke(classOf[scala.collection.immutable.StringOps], "toInt")
+        }
         case _ => super.emitNode(sym, rhs)
     }
   }
